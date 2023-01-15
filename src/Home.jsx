ca4f634319c2c8b4d994e2657f97e921433dadfd
Barnabas5035 <110441604+Barnabas5035@users.noreply.github.com>
2023-01-15 08:15:04 -0800

    changes component

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,22 +11,28 @@ const Home = () => {
   console.log(CartProductId)
   return (
     <div className='bg-white-200   p-6   '>
-      <div className=' grid grid-cols-3 gap-x-4 gap-y-8  mt-8  '>
+      <div className='sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  grid grid-cols-3 gap-x-4 gap-y-8  mt-8  '>
         {product.map((data) => {
           const { id, imageUrl, price, name } = data
           return (
             <div key={id} className='  shadow-md w-7/12 ml-10 py-4 '>
               <div className=' item-center '>
-                <img src={imageUrl} alt='pic' className='w-full ' />
+                <img
+                  src={imageUrl}
+                  alt='pic'
+                  className='sm:w-full md:w-full  lg:w-full'
+                />
               </div>
-              <div className='sm:text-center md:text-center lg:text-center'>
+              <div className='sm:flex-jusify-center text-center md:flex-jusify-center text-center lg: flex-jusify-center text-center'>
                 <h5 className='mt-2 text-black text-lg font-medium font-serif'>
                   {name}
                 </h5>
-                <p className='mt-3 text-black text-lg'>${price}</p>
+                <p className='sm:text-center md:text-center lg:text-center mt-3 text-black text-lg'>
+                  ${price}
+                </p>
                 {!CartProductId.includes(data.id) && (
                   <button
-                    className=' bg-sky-500/100 rounded py-2 px-2.5 mt-1 '
+                    className='sm:text-center md:text-center lg:text-center bg-sky-500/100 rounded py-2 px-2 mt-1 '
                     onClick={() => dispatch(addToCart(data.id))}
                   >
                     addToCart
@@ -34,7 +40,7 @@ const Home = () => {
                 )}
                 {CartProductId.includes(data.id) && (
                   <button
-                    className=' bg-sky-500/100 rounded py-2 px-2.5 mt-1 '
+                    className='sm:text-center bg-sky-500/100 rounded py-2 px-2 mt-1 '
                     onClick={() => dispatch(removeCart(data.id))}
                   >
                     removeCart
